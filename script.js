839917577cd2ed35fba25a7777b21ececde763df
Kateryna Masalska <katerynamasalska@gmail.com>
2023-11-11 22:01:49 +0200

    done

--- script.js
+++ script.js
@@ -122,47 +122,43 @@ class User{
 
 			const divUserInfoData = createDivWithClass("user__info--data");
 
-			const userImg = createImageWithAlt(this.img, this.name);
-			divUserInfoData.appendChild(userImg);
+				const userImg = createImageWithAlt(this.img, this.name);
+				divUserInfoData.appendChild(userImg);
 
-			const divUserNaming = createDivWithClass("user__naming");
-			const userName = createParagraph(`Name: <strong>${this.name}</strong>`);
-			const userAge = createParagraph(`Age: <strong>${this.age}</strong>`);
-			divUserNaming.appendChild(userName);
-			divUserNaming.appendChild(userAge);
-			
+				const divUserNaming = createDivWithClass("user__naming");
+				const userName = createParagraph(`Name: <strong>${this.name}</strong>`);
+				const userAge = createParagraph(`Age: <strong>${this.age}</strong>`);
+				divUserNaming.appendChild(userName);
+				divUserNaming.appendChild(userAge);
 
-			const divUserInfoRole = createDivWithClasses([this.role, "user__info--role"]);
-			const imgRole = createImageWithAlt(roles[this.role], this.role);
-			const userRole = createParagraph(this.role);
-			divUserInfoRole.appendChild(imgRole);
-			divUserInfoRole.appendChild(userRole);
+				const divUserInfoRole = createDivWithClasses([this.role, "user__info--role"]);
+				const imgRole = createImageWithAlt(roles[this.role], this.role);
+				const userRole = createParagraph(this.role);
+				divUserInfoRole.appendChild(imgRole);
+				divUserInfoRole.appendChild(userRole);
 
 			divUserInfoData.appendChild(divUserNaming);
-
-			const divUserCurses = createDivWithClass("user__courses");
-			if (this.courses) {
-				this.courses.forEach((course) => {
-				const userCourse = createParagraph("", [this.role, "user__courses--course"]);
-
-
-				/////////////////////////////////
-				const userCourseRes = course.mark;
-				console.log(gradation);
-				userCourse.innerHTML = `${course.title} <span class="${userCourseRes}">${userCourseRes}</span>`;
-				divUserCurses.appendChild(userCourse);
-				});
-			}
 
 			divUserInfo.appendChild(divUserInfoData);
 			divUserInfo.appendChild(divUserInfoRole);
 
 			divUser.appendChild(divUserInfo);
-			divUser.appendChild(divUserCurses);
+			this.renderCourses(divUser);
 			divUsers.appendChild(divUser);
     }
 
-    renderCourses(){
+    renderCourses(divUser){
+		const divUserCurses = createDivWithClass("user__courses");
+		if (this.courses) {
+			this.courses.forEach((course) => {
+				const userCourse = createParagraph("", [this.role, "user__courses--course"]);
+
+				const userCourseRes = calculateScore(course.mark);
+				userCourse.innerHTML = `${course.title} <span class="${userCourseRes}">${userCourseRes}</span>`;
+				divUserCurses.appendChild(userCourse);
+			});
+		}
+		divUser.appendChild(divUserCurses);
     }
 }
 
@@ -192,13 +188,42 @@ function createDivWithClasses(classList) {
 	return div;
 }
 
+function calculateScore(mark) {
+	for (let key in gradation) {
+		if (mark <= parseInt(key)) {
+		  	return gradation[key];
+		}
+	}
+}
+
   
 class Admin extends User{
     constructor(name, age, img, role, courses){
         super(name, age, img, role, courses);
     }
 
-    renderCourses(){
+    renderCourses(divUser){
+		const divUserCurses = createDivWithClasses(["user__courses", "admin--info"]);
+		if (this.courses) {
+			this.courses.forEach((course) => {
+				const divUserCurs = createDivWithClasses(["user__courses--course", this.role]);
+
+				const pTitle = document.createElement("p");
+				pTitle.innerHTML = `Title: <strong>${course.title}</strong>`;
+
+				const pScore = document.createElement("p");
+				pScore.innerHTML = `Admin's score: <span class="${calculateScore(course.score)}">${calculateScore(course.score)}</span>`;
+
+				const pLector = document.createElement("p");
+				pLector.innerHTML = `Lector: <strong>${course.lector}</strong>`;
+				
+				divUserCurs.appendChild(pTitle);
+				divUserCurs.appendChild(pScore);
+				divUserCurs.appendChild(pLector);
+				divUserCurses.appendChild(divUserCurs);
+			});
+		}
+		divUser.appendChild(divUserCurses);
     }
 }
 
@@ -207,7 +232,28 @@ class Lector extends User{
         super(name, age, img, role, courses);
     }
 
-    renderCourses(){
+    renderCourses(divUser){
+		const divUserCurses = createDivWithClasses(["user__courses", "admin--info"]);
+		if (this.courses) {
+			this.courses.forEach((course) => {
+				const divUserCurs = createDivWithClasses(["user__courses--course", this.role]);
+
+				const pTitle = document.createElement("p");
+				pTitle.innerHTML = `Title: <strong>${course.title}</strong>`;
+
+				const pScore = document.createElement("p");
+				pScore.innerHTML = `Lector's score: <span class="${calculateScore(course.score)}">${calculateScore(course.score)}</span>`;
+
+				const pStudentsScore = document.createElement("p");
+				pStudentsScore.innerHTML = `Average student's score: <span class="${calculateScore(course.studentsScore)}">${calculateScore(course.studentsScore)}</span>`;
+				
+				divUserCurs.appendChild(pTitle);
+				divUserCurs.appendChild(pScore);
+				divUserCurs.appendChild(pStudentsScore);
+				divUserCurses.appendChild(divUserCurs);
+			});
+		}
+		divUser.appendChild(divUserCurses);
     }
 }
 
@@ -219,5 +265,9 @@ class Student extends User{
 
 
 users.map((user) => {
-    return new User(user.name, user.age, user.img, user.role, user.courses);
+	switch(user.role){
+		case "student": return new Student(user.name, user.age, user.img, user.role, user.courses);
+		case "lector": return new Lector(user.name, user.age, user.img, user.role, user.courses);
+		case "admin": return new Admin(user.name, user.age, user.img, user.role, user.courses);
+	}
 }).forEach((user) => user.render());
